c9e2c19c0868e30fa5f4ba14b0fb653d06e71d92
himcraft <1014harry@gmail.com>
2023-12-28 12:25:55 +0800

    ignore vector field for now

--- src/fieldReader.jl
+++ src/fieldReader.jl
@@ -2,15 +2,19 @@
 function internalFieldReader(Case::FOAMCase)
     fieldsIndex=Dict(i => Array{Float64}(undef,Case.timeLength,Case.cells) for i in Case.fieldList)
     for (time,snapshot) in enumerate(Case.timeSequence)
-        for field in Case.fieldList
+        for (pos,field) in enumerate(Case.fieldList)
             if isfile(Case.case*"/"*snapshot*"/"*field * (Case.gz ? ".gz" : ""))
                 fileContent=foamOpen(Case.case*"/"*snapshot*"/"*field,Case)
-                if split(fileContent[20])[2]=="nonuniform"
-                    fieldsIndex[field][time,:]=str2flt.(fileContent[23:22+Case.cells])
-                elseif split(fileContent[20])[2]=="uniform"
-                    fieldsIndex[field][time,:].=str2flt(replace(split(fileContent[20])[3],";"=>""))
-                else
-                    error("error in reading field data")
+                if Case.fieldType[pos]==:volScalarField
+                    if split(fileContent[20])[2]=="nonuniform"
+                        fieldsIndex[field][time,:]=str2flt.(fileContent[23:22+Case.cells])
+                    elseif split(fileContent[20])[2]=="uniform"
+                        fieldsIndex[field][time,:].=str2flt(replace(split(fileContent[20])[3],";"=>""))
+                    else
+                        error("error in reading field data")
+                    end
+                elseif Case.fieldType[pos]==:volVectorField
+                    continue
                 end
             else
                 fieldsIndex[field][time,:].=0
