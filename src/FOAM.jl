b6924e516fac0048e79bf917a29708f5af2f0948
himcraft <1014harry@gmail.com>
2024-01-01 18:29:03 +0800

    add export functions

--- src/FOAM.jl
+++ src/FOAM.jl
@@ -4,10 +4,15 @@ using GZip
 using HDF5
 
 export FOAMCase
+export internalFieldsReader
 export internalFieldReader
+export H5internalFieldSaver
+export H5internalFieldReader
 # export boundaryFieldReader
 export printCaseSettings
 export printFieldNames
+export writeFOAMCase
+export readFOAMCase
 
 include("utilities.jl")
 include("fieldReader.jl")
